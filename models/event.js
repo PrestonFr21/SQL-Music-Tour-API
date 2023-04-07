1efe868d49a38803eab3163bf10cb9295be10c26
PrestonFr <PrestonFr@gmail.com>
2023-04-07 11:29:19 -0400

    typo error fixed within meet-greet model and event model updated

--- models/event.js
+++ models/event.js
@@ -1,37 +1,62 @@
-// DEPENDENCIES
-const { Sequelize, DataTypes,  Model } = require('sequelize')
+'use strict'
+const {
+  Model
+} = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Event extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ Stage, StageEvent, MeetGreet, SetTime }) {
+      // stages
+      Event.belongsToMany(Stage, {
+        foreignKey: "event_id",
+        as: "stages",
+        through: StageEvent
+      })
+      
+      // meet and greets 
+      Event.hasMany(MeetGreet, {
+        foreignKey: "event_id",
+        as: "meet_greets"
+      })
 
-// MODEL
-class Band extends Model{}
-
-Band.init ({
-    band_id: {
-        type: DataTypes.INTERGER,
-        primaryKey: true,
-        autoIncrement: true
+      // set times 
+      Event.hasMany(SetTime, {
+        foreignKey: "event_id",
+        as: "set_times"
+      })
+    }
+  }
+  Event.init({
+    event_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
     },
     name: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false
     },
-    genre: {
-        type: DataTypes.TEXT,
-        allowNull: false
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false
     },
-    available_start_time: {
-        type: DataTypes.DATE,
-        allowNull: false
+    start_time: {
+      type: DataTypes.DATE,
+      allowNull: false
     },
     end_time: {
-        type: DataTypes.DATE,
-        allowNull: false
-    },
-}, {
+      type: DataTypes.DATE,
+      allowNull: false
+    }
+  }, {
     sequelize,
-    modelName: 'Band',
-    tableName: 'band',
-    timestamp: false
-})
-
-// EXPORT
-module.exports = Band
+    modelName: 'Event',
+    tableName: 'events',
+    timestamps: false
+  })
+  return Event
+}
